1ba66ad62ec3ad40b697e7aeff0e44add07e1d5e
Harinderpal singh <hsbhular@gmail.com>
2025-09-13 11:38:12 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -3,8 +3,6 @@ import os
 from os import getenv, environ
 from dotenv import load_dotenv
 
-
-
 load_dotenv()
 
 class Var(object):
@@ -17,26 +15,32 @@ class Var(object):
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1003016985569'))
     PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '194.87.18.248'))
+    
+    # Your VPS IP address
+    VPS_IP = '194.87.18.248'
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', VPS_IP))
+    
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6678306923").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'wipro_x'))
+    
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('File2link'))
-    
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', '194.87.18.248:8080')) if not ON_HEROKU or getenv('FQDN', '194.87.18.248:8080') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
+    
+    # Use your VPS IP for FQDN when not on Heroku
+    FQDN = str(getenv('FQDN', f'{VPS_IP}:{PORT}')) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    
+    HAS_SSL = bool(getenv('HAS_SSL', False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
+        
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://falvo:FShMB8ufwkL0Gvt5@cluster0.9kut1hj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'GBEXTREM'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-10013626597")).split())) 
-    
-    
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-10013626597")).split()))
